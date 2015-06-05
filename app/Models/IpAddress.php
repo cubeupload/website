b56cb90f8141d0fe687443a1881943618dc71588
Tom Dicks <tom@ubermensch.tv>
2015-06-05 12:31:54 +0100

    Update IpAddress.php

--- app/Models/IpAddress.php
+++ app/Models/IpAddress.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class IpAddress extends Model {
 
+	public protected $fillable = ['decimal', 'country'];
+
 	public function user()
 	{
 		return $this->belongsToMany('App\Models\User');
