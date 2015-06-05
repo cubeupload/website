ef8f826b24b20685db9dffe9b743f2ab546daab1
Tom Dicks <tom@ubermensch.tv>
2015-06-05 12:06:37 +0100

    Update 2015_06_04_221813_create_ip_address_user_table.php

--- database/migrations/2015_06_04_221813_create_ip_address_user_table.php
+++ database/migrations/2015_06_04_221813_create_ip_address_user_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateIpAddressesUsersTable extends Migration {
+class CreateIpAddressUserTable extends Migration {
 
 	/**
 	 * Run the migrations.
