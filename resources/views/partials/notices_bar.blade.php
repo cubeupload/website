316629efdfb0b5ad68233a6155b21fd9e4c6823a
Tom Dicks <tom@ubermensch.tv>
2015-09-07 21:43:37 +0100

    Removed title from notice bar

--- resources/views/partials/notices_bar.blade.php
+++ resources/views/partials/notices_bar.blade.php
@@ -3,11 +3,10 @@
 	<div class="row">
 		@foreach( $notices as $notice )
 		<div class="col-md-12">
-			<div class="alert alert-{{ $notice->type }}" role="alert">
+			<div class="alert alert-{{ $notice->style }}" role="alert">
 				@if( $notice->dismissable )
 				<button type="button" class="close noticeClose" data-dismiss="alert" data-notice-id="{{ $notice->id }}" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 				@endif
-				<strong>{{ $notice->title }}</strong>
 				{!! $notice->text !!}
 			</div>
 		</div>
