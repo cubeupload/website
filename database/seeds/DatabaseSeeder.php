abda5179011b2050f77fd5a30517973faf0f209c
Tom Dicks <tom@ubermensch.tv>
2015-09-07 21:45:32 +0100

    Added notices table seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+
 use App\Models\User;
+use App\Models\Notice;
 
 class DatabaseSeeder extends Seeder {
 
@@ -16,6 +18,7 @@ class DatabaseSeeder extends Seeder {
 		Model::unguard();
 
 		$this->call('UserTableSeeder');
+		$this->call('NoticeTableSeeder');
 	}
 
 }
@@ -29,10 +32,30 @@ class UserTableSeeder extends Seeder
 		User::create(
 			[
 				'username' => 'sysadmin',
-				'email' => 'staff@cubeupload.com',
+				'email' => 'sysadmin@cubeupload.com',
 				'name' => 'System Admin',
 				'level' => 9,
 				'password' => 'cubeupload'
+			]
+		);
+	}
+}
+
+class NoticeTableSeeder extends Seeder
+{
+	public function run()
+	{
+		DB::table('notices')->delete();
+
+		Notice::create(
+			[
+				'title' => 'New Install Notice',
+				'text' => '<strong>Hi!</strong> This is a new install. Login as sysadmin@cubeupload.com with the password "cubeupload" to get started.',
+				'style' => 'success',
+				'show_to' => 'all',
+				'visible' => 1,
+				'dismissable' => 1,
+				'metric' => 0
 			]
 		);
 	}
