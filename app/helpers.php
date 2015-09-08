46ea8cc25c4ebdc37762fedc909bf469668b0a69
Tom Dicks <tom@ubermensch.tv>
2015-09-08 12:05:40 +0100

    Added helpers library & updated composer

---
+++ app/helpers.php
@@ -0,0 +1,8 @@
+<?php
+
+	// http://jeffreysambells.com/2012/10/25/human-readable-filesize-php
+	function human_filesize($bytes, $decimals = 2) {
+	    $size = array('B','kB','MB','GB','TB','PB','EB','ZB','YB');
+	    $factor = floor((strlen($bytes) - 1) / 3);
+	    return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$size[$factor];
+	}
